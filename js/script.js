1b2861f222d5abb990c7f0a332b63fdbd4f2d0f2
Norbert Mszyca <norbi983@interia.pl>
2022-10-14 15:58:46 +0200

    added button

--- js/script.js
+++ js/script.js
@@ -1,15 +1,36 @@
-{
-    const tasks = [];
+const tasks = [];
 
-    const addNewTask = (newTaskContent) => {
-        tasks.push({content: newTaskContent});
-        render();
-    };
-
-    
-
-
-        
-
-
+const addNewTask = (newTaskContent) => {
+    tasks.push({ content: newTaskContent });
+    render();
 };
+
+const removeTask = (taskIndex) => {
+    tasks.splice(taskIndex, 1);
+    render();
+};
+
+const toogleTaskDone = (taskIndex) => {
+    tasks[taskIndex].done = !tasks[taskIndex].done;
+    render;
+};
+
+const bindEvent = () => {
+    const removeButton = document.querySelectorAll(".js-remove");
+
+    removeButton.forEach((removeButton, taskIndex) => {
+        removeButton.addEventListener("click", () => {
+            removeTask(taskIndex);
+        });
+    });
+
+    const toggleDoneButton = document.querySelectorAll(".js-toggleDone");
+
+    toggleDoneButton.forEach((toggleDoneButton, taskIndex) => {
+        toggleDoneButton.addEventListener("click", () => {
+            toogleTaskDone(taskIndex);
+        });
+    });
+};
+
+
