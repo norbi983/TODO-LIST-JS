4349e8efd43df8f931854809a0affe2954551f66
Norbert Mszyca <norbi983@interia.pl>
2022-11-07 16:44:22 +0100

    new task

--- js/script.js
+++ js/script.js
@@ -44,11 +44,11 @@
 
         for (const task of tasks) {
             htmlString += `
-                <li class="tasks__item js-task">
+                <li class="tasks__item js-tasks">
                      <button class="tasks__button tasks__button--toggleDone js-toggleDone">
                         ${task.done ? "✔️" : ""}
                  </button>
-                    <span class="tasks__content${task.done ? "tasks__content--done" : ""}">
+                    <span class="${task.done ? "tasks__content--done" : ""}">
                      ${task.content}
                  </span>
                     <button class="tasks__button tasks__button--remove js-remove">
@@ -82,8 +82,8 @@
         render();
         welcome();
 
-        const from = document.querySelector(".js-form");
-        from.addEventListener("submit", onFormSubmint);
+        const form = document.querySelector(".js-form");
+        form.addEventListener("submit", onFormSubmint);
 
     };
 
